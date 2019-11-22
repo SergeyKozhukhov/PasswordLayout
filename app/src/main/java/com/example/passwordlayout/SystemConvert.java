a065a2b698a5a22eb2e3da084a08420dacd8b6e2
SergeyKozhukhov <sergey.kozhukhov.mail@mail.ru>
2019-11-22 15:05:15 +0300

    Initial commit

---
+++ app/src/main/java/com/example/passwordlayout/SystemConvert.java
@@ -0,0 +1,39 @@
+package com.example.passwordlayout;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SystemConvert {
+
+    private final Map<Character, Character> dictConvert;
+
+
+    public SystemConvert(String firstLang, String secondLang) {
+
+        if(firstLang.length() != secondLang.length())
+            throw new IllegalArgumentException();
+
+        this.dictConvert = new HashMap<>(firstLang.length());
+
+        for (int i = 0; i < firstLang.length(); i++)
+            dictConvert.put(firstLang.charAt(i), secondLang.charAt(i));
+    }
+
+    public String convert (CharSequence source)
+    {
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < source.length(); i++)
+        {
+            Character symbol = dictConvert.get(Character.toLowerCase(source.charAt(i)));
+
+            if (symbol != null)
+                result.append(Character.isUpperCase(source.charAt(i)) ? Character.toUpperCase(symbol) : symbol);
+            else
+                result.append(source.charAt(i));
+        }
+
+        return result.toString();
+    }
+
+}
